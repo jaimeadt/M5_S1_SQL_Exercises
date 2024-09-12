2d4577c4168452570a6add8b36886ace392b0a0a
jaimeadt <jaime.adt@gmail.com>
2024-09-12 17:13:21 +0200

    Version_Final

--- Ejercicio1.sql
+++ Ejercicio1.sql
@@ -117,3 +117,40 @@ LEFT JOIN pedidos
 ON clientes.id = pedidos.cliente_id
 GROUP BY clientes.nombre
 
+/* 17. Realizar una consulta que muestre el nombre del producto y la cantidad total de
+pedidos de ese producto*/
+
+SELECT producto, SUM(cantidad) FROM pedidos
+GROUP BY producto
+
+/* 18. Agregar una columna llamada "fecha" a la tabla "Pedidos" de tipo fecha.*/
+
+ALTER TABLE pedidos
+ADD COLUMN fecha DATE
+
+/* 19. Agregar una clave externa a la tabla "Pedidos" que haga referencia a la tabla
+"Productos" en la columna "producto".*/
+
+	/* Para poder hacerlo primero hay que restringir que el campo "nombre" de la tabla "productos" son valores únicos por consistenca e integridad*/
+
+	ALTER TABLE productos
+ADD CONSTRAINT unique_nombre UNIQUE (nombre)
+
+	/* Posteriormente ya se puede referenciar la FK de la tabla "pedidos" a la tabla de "productos".*/
+
+	ALTER TABLE pedidos
+ADD CONSTRAINT fk_producto
+FOREIGN KEY (producto) REFERENCES productos (nombre)
+
+/* 20. Realizar una consulta que muestre los nombres de los clientes, los nombres de
+los productos y las cantidades de los pedidos donde coincida la clave externa.*/
+
+SELECT
+clientes.nombre,
+productos.nombre,
+pedidos.cantidad
+FROM pedidos
+INNER JOIN clientes
+ON pedidos.cliente_id = clientes.id
+INNER JOIN productos
+ON pedidos.producto = productos.nombre
